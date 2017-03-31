cc27fc897438dbd9ccade7781dd8738ce31049a0
Santiago Cabrera <scabfos@gmail.com>
2017-03-31 13:29:19 +0200

    axios promises

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,30 +1,26 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes : {
         router : React.PropTypes.object.isRequired
     },
     getInitialState : function(){
-        console.log("getInitialState");
         return {
             isLoading: true,
             playersInfo: []
         }
     },
-    componentWillMount: function(){
-        console.log("componentWillMount");
-    },
     componentDidMount: function(){
         var query = this.props.location.query;
-        console.log("componentDidMount");
-        //fetch info from Github then update state
-    },
-    componentWillReceiveProps: function(){
-        console.log("componentWillReceiveProps");
-    },
-    componentWillUnmount: function(){
-        console.log("componentWillUnmount");
+        githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+        .then(function(players){
+            this.setState({
+                isLoading: false,
+                playersInfo: [players[0], players[1]]
+            })
+        }.bind(this));
     },
     render: function(){
         return (
